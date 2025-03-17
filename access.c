a111843f7b7879c8af031590b52de6142eb026d6
Shravan <mukkawarshravan04@gmail.com>
2025-03-17 19:01:17 +0530

    Check test.c and access.c

--- access.c
+++ access.c
@@ -4,7 +4,7 @@
 int main(){
 
 
-	char *shm=shmat(98359,NULL,0);
+	char *shm=shmat(229433,NULL,0);
 	printf("Data written in our memory is %s",shm);
 	
 	return 0;
--- test.c
+++ test.c
@@ -1,18 +1,51 @@
-#include<stdio.h>
-#include<sys/shm.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <string.h>
 
-	int idx=shmget(1234,1024,IPC_CREAT | 0666);
-	if(idx==-1){
-		perror("shmget"); 
-        	return 1;
+int create_memory(int key, int size) {
+	int idx = shmget(key, size, IPC_CREAT | 0666);
+	if (idx == -1) {
+		perror("shmget");
+		return -1;
 	}
-        printf("Shared memory segment created with ID: %d\n", idx);
+	return idx;
+}
 
-	char *shm=shmat(idx,NULL,0);
-	char *messg="Hello Shravan";
-	strcpy(shm,messg);
-	
+void attachfile(int idx) {
+	FILE *file = fopen("shravan.txt", "r");
+	if (file == NULL) {
+		perror("fopen");
+		return;
+	}
+
+	char *shm = shmat(idx, NULL, 0);
+	if (shm == (char *)-1) {
+		perror("shmat");
+		fclose(file);
+		return;
+	}
+
+	char buffer[1024];
+	size_t bytes = fread(buffer, sizeof(char), 1023, file);
+	buffer[bytes] = '\0';
+	fclose(file);
+
+	strncpy(shm, buffer, 1024);
+	printf("Data copied to shared memory: %s\n", shm);
+	if (shmdt(shm) == -1) {
+		perror("shmdt");
+	}
+}
+
+int main() {
+	int idx = create_memory(119, 1024);
+	if (idx == -1) {
+		return 1;
+	}
+
+	printf("Memory Segment Key is: %d\n", idx);
+	attachfile(idx);
+
 	return 0;
-	}
+}
